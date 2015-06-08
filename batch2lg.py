2b25af5b510342f3cf88bb118b994ed70d990ec6
Andy Terkik <aterkik@gmail.com>
2015-06-07 20:35:09 -0700

    move all code to separate directory

--- batch2lg.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-filelist = os.listdir(sys.argv[1])
-
-path = sys.argv[1].rstrip("/") + "/"
-for fname in filelist:
-    if fname.endswith('inkml'):
-        os.system('crohme2lg %s%s %s%s' % (path, fname, path, fname.rstrip("inkml") + "lg"))
